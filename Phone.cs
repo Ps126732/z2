db8d985746149bc6c6b151b20624394133bf1217
Sebastian Cieślik <sebastian.cieslik@microsoft.wsei.edu.pl>
2026-03-13 17:16:50 +0100

    dodanie pomyślnego testu konstruktora.
    Poprawa struktury i formatowania klasy Phone
    
    Przeniesiono całą logikę do wnętrza klasy Phone, poprawiono wcięcia, deklaracje pól, właściwości i metod. Zmiany mają charakter porządkujący i nie wpływają na działanie kodu.

--- Phone.cs
+++ Phone.cs
@@ -3,77 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace z2;
-
-public class Phone
 {
-    private string owner;
-    // string of 9 digits
-    private string phoneNumber;
-    // Dictionary of <name, number>
-    private readonly Dictionary<string, string> phoneBook;
-    public int PhoneBookCapacity => 100;
-
-    public Phone(string owner, string phoneNumber)
+    public class Phone
     {
-        Owner = owner;
-        PhoneNumber = phoneNumber;
-        phoneBook = new Dictionary<string, string>();
-    }
+        private string owner;
+        // string of 9 digits
+        private string phoneNumber;
+        // Dictionary of <name, number>
+        private readonly Dictionary<string, string> phoneBook;
+        public int PhoneBookCapacity => 100;
 
-    public string Owner
-    {
-        get => owner;
-        private set
+        public Phone(string owner, string phoneNumber)
         {
-            if (string.IsNullOrEmpty(value))
-                throw new ArgumentException($"Owner name is empty or null!");
-
-            owner = value;
-        }
-    }
-
-    private bool IsCorrectPhoneNumber(string number)
-        => (number != null) && number.Length == 9 && number.All(c => char.IsDigit(c));
-
-    public string PhoneNumber
-    {
-        get => phoneNumber;
-        private set
-        {
-            if (string.IsNullOrEmpty(value))
-                throw new ArgumentException($"Phone number is empty or null!");
-
-            if (!IsCorrectPhoneNumber(value))
-                throw new ArgumentException($"Invalid phone number!");
-
-            phoneNumber = value;
-        }
-    }
-
-    public int Count => phoneBook.Count;
-
-    public bool AddContact(string name, string number)
-    {
-        if (Count == PhoneBookCapacity)
-            throw new InvalidOperationException("Phonebook is full!");
-
-        if (!phoneBook.ContainsKey(name))
-        {
-            phoneBook.Add(name, number);
-            return true;
+            Owner = owner;
+            PhoneNumber = phoneNumber;
+            phoneBook = new Dictionary<string, string>();
         }
 
-        return false;
-    }
-
-    public string Call(string name)
-    {
-        if (phoneBook.ContainsKey(name))
+        public string Owner
         {
-            var number = phoneBook[name];
-            return $"Calling {number} ({name}) ...";
+            get => owner;
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException($"Owner name is empty or null!");
+
+                owner = value;
+            }
         }
 
-        throw new InvalidOperationException("Person doesn't exists!");
+        private bool IsCorrectPhoneNumber(string number)
+            => (number != null) && number.Length == 9 && number.All(c => char.IsDigit(c));
+
+        public string PhoneNumber
+        {
+            get => phoneNumber;
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new ArgumentException($"Phone number is empty or null!");
+
+                if (!IsCorrectPhoneNumber(value))
+                    throw new ArgumentException($"Invalid phone number!");
+
+                phoneNumber = value;
+            }
+        }
+
+        public int Count => phoneBook.Count;
+
+        public bool AddContact(string name, string number)
+        {
+            if (Count == PhoneBookCapacity)
+                throw new InvalidOperationException("Phonebook is full!");
+
+            if (!phoneBook.ContainsKey(name))
+            {
+                phoneBook.Add(name, number);
+                return true;
+            }
+
+            return false;
+        }
+
+        public string Call(string name)
+        {
+            if (phoneBook.ContainsKey(name))
+            {
+                var number = phoneBook[name];
+                return $"Calling {number} ({name}) ...";
+            }
+
+            throw new InvalidOperationException("Person doesn't exists!");
+        }
     }
 }
