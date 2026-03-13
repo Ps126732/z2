efedb313e3d97266115e4873be62d8b6443477a4
Ps126732 <sebastian.cieslik@microsoft.wsei.edu.pl>
2026-03-13 18:13:18 +0100

    Add files via upload

---
+++ z2.Tests/PhoneTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using NUnit.Framework;
+
+
+namespace z2.Tests
+{
+    [TestFixture]
+
+    public class PhoneTests
+    {
+        [Test]
+        public void Constructor_PoprawneDane_TworzyObiekt()
+        {
+            string wlasciciel = "Jan Kowalski";
+            string numer = "123456789";
+
+
+            var telefon = new Phone(wlasciciel, numer);
+
+
+            Assert.That(telefon.Owner, Is.EqualTo(wlasciciel));
+            Assert.That(telefon.PhoneNumber, Is.EqualTo(numer));
+            Assert.That(telefon.Count, Is.EqualTo(0));
+            Assert.That(telefon.PhoneBookCapacity, Is.EqualTo(100));
+        }
+
+        [Test]
+        public void Constructor_RzucaWyjatek()
+        {
+            Assert.Throws<ArgumentException>(() => new Phone(null, "123456789"));
+            Assert.Throws<ArgumentException>(() => new Phone("", "123456789"));
+            Assert.Throws<ArgumentException>(() => new Phone("Jan Kowalski", null));
+            Assert.Throws<ArgumentException>(() => new Phone("Jan Kowalski", ""));
+            Assert.Throws<ArgumentException>(() => new Phone("Jan Kowalski", "12345"));
+            Assert.Throws<ArgumentException>(() => new Phone("Jan Kowalski", "1234567890"));
+            Assert.Throws<ArgumentException>(() => new Phone("Jan Kowalski", "12345678a"));
+        }
+
+        [Test]
+        public void AddContact_PojemnoscMaksymalna_RzucaWyjatek()
+        {
+            var telefon = new Phone("Jan", "123456789");
+
+
+            for (int i = 0; i < 100; i++)
+            {
+                telefon.AddContact("Osoba" + i, "111222333");
+            }
+        }
+        
+        }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
